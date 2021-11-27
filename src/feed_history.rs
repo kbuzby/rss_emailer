597856f052aaec0a80e30274459b2558d940e28e
Kyle Buzby <kyle@buzby.dev>
2021-11-27 10:57:31 -0600

    add history tracking

---
+++ src/feed_history.rs
@@ -0,0 +1,62 @@
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::path::Path;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FeedHistory {
+    path: String,
+    feed_items: HashMap<String, HashSet<String>>,
+}
+
+impl FeedHistory {
+    pub fn new(path: &str) -> FeedHistory {
+        FeedHistory {
+            path: path.to_string(),
+            feed_items: HashMap::new(),
+        }
+    }
+
+    pub fn get_or_create(path: &str) -> FeedHistory {
+        match FeedHistory::get_from_file(path) {
+            Some(history) => history,
+            _ => FeedHistory::new(path),
+        }
+    }
+
+    fn get_from_file(path: &str) -> Option<FeedHistory> {
+        if Path::new(path).exists() {
+            let config_file = fs::read_to_string(path);
+            return Some(serde_json::from_str::<FeedHistory>(&config_file.unwrap()).unwrap());
+        }
+        return None;
+    }
+
+    pub fn save_to_disk(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let string = serde_json::to_string_pretty(self)?;
+        let _ = fs::write(&self.path, string)?;
+        Ok(())
+    }
+
+    pub fn item_sent(&self, feed_name: &str, item_name: &str) -> bool {
+        match self.feed_items.get(feed_name) {
+            Some(items) => items.contains(item_name),
+            _ => false,
+        }
+    }
+
+    pub fn track_item(&mut self, feed_name: &str, item_name: &str) {
+        self.ensure_feed_history_exists(feed_name);
+        self.feed_items
+            .get_mut(feed_name)
+            .unwrap()
+            .insert(item_name.to_string());
+    }
+
+    fn ensure_feed_history_exists(&mut self, feed_name: &str) {
+        if !self.feed_items.contains_key(feed_name) {
+            self.feed_items
+                .insert(feed_name.to_string(), HashSet::new());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod feed_history;
 mod feed_mailer;
 mod rss_emailer_config;
 
+use feed_history::FeedHistory;
 use feed_mailer::FeedMailer;
-use rss::Channel;
+use rss::{Channel, Item};
 use rss_emailer_config::FeedDefinition;
 use std::path::PathBuf;
 
@@ -10,6 +12,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Some(config_path) = get_config_path() {
         let config = rss_emailer_config::read_from_file(config_path.to_str().unwrap());
         let mut mailer = FeedMailer::new(&config);
+        let mut history = FeedHistory::get_or_create(get_history_path().unwrap().to_str().unwrap());
         for (feed_name, feed_def) in config.feeds {
             if let FeedDefinition::Simple(feed_link) = feed_def {
                 let content = reqwest::blocking::get(feed_link)?.bytes()?;
@@ -19,19 +22,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 } else {
                     &feed_name
                 };
-                if let Ok(_) = mailer.send_email(title, &channel.items[0]) {}
+                send_if_new(&mut history, &mut mailer, title, &channel.items[0]);
             } else if let FeedDefinition::Detailed(_feed_detail) = feed_def {
                 /* TODO */
             }
         }
+        let _ = history.save_to_disk()?;
         return Ok(());
     }
     panic!("Unable to get config file.");
 }
 
+fn send_if_new(history: &mut FeedHistory, mailer: &mut FeedMailer, feed_name: &str, item: &Item) {
+    let item_title = item.title.as_ref().unwrap();
+    if !history.item_sent(feed_name, item_title) {
+        if let Ok(_) = mailer.send_email(feed_name, item) {
+            history.track_item(feed_name, item_title);
+        }
+    }
+}
+
 fn get_config_path() -> Option<PathBuf> {
     if let Some(home_path) = home::home_dir() {
         return Some(home_path.join(".config/rss_emailer/config"));
+    }
+    return None;
+}
+
+fn get_history_path() -> Option<PathBuf> {
+    if let Some(home_path) = home::home_dir() {
+        return Some(home_path.join(".config/rss_emailer/history"));
     }
     return None;
 }
