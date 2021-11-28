9dec34294f7930d398856c38b52525a873de1c85
Kyle Buzby <kyle@buzby.dev>
2021-11-28 10:00:11 -0600

    add feed history loggin

--- src/feed_history.rs
+++ src/feed_history.rs
@@ -1,3 +1,4 @@
+use log::{debug, info};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 use std::fs;
@@ -18,21 +19,28 @@ impl FeedHistory {
     }
 
     pub fn get_or_create(path: &str) -> FeedHistory {
+        debug!("Attempting to get or create feed history.");
         match FeedHistory::get_from_file(path) {
             Some(history) => history,
-            _ => FeedHistory::new(path),
+            _ => {
+                info!("Creating new feed history.");
+                FeedHistory::new(path)
+            }
         }
     }
 
     fn get_from_file(path: &str) -> Option<FeedHistory> {
         if Path::new(path).exists() {
+            info!("Reading feed history from {}.", path);
             let config_file = fs::read_to_string(path);
             return Some(serde_json::from_str::<FeedHistory>(&config_file.unwrap()).unwrap());
         }
+        info!("Feed history does not exist.");
         return None;
     }
 
     pub fn save_to_disk(&self) -> Result<(), Box<dyn std::error::Error>> {
+        info!("Saving feed history to {}.", &self.path);
         let string = serde_json::to_string_pretty(self)?;
         let _ = fs::write(&self.path, string)?;
         Ok(())
